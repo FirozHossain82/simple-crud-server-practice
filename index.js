76d12b183c2f7067914b03abb0ed0a882db1fdff
FirozHossain82 <firozhossain295@gmail.com>
2023-10-21 12:35:12 +0600

    creat the data by database

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // simpleCrud
 // kmMPDSuIBSR4jFB6
@@ -26,12 +29,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db('crudDB')
+    const userCollection = database.collection('users');
+
+// create post
+    app.post('/users', async(req, res) =>{
+         const user = req.body;
+         console.log('new user', user);
+         const result = await userCollection.insertOne(user);
+         res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
